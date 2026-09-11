ae04022a8036e97c91b595b9ddc6ba2aef6b4ede
28-KrishnaGhosh <krishna349ghosh@gmail.com>
2026-09-11 19:09:06 +0530

    Fix: correct folder structure

---
+++ src/test/java/com/prafull/flightbooking/Flight_Booking_System/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prafull.flightbooking.Flight_Booking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
